e12da1ba848ed1cafc661f240b335ffc78395919
Joshi <parnal@iastate.edu>
2021-11-03 12:56:58 -0500

    Directory restructuring, adding setup files for python packaging, and changes to create_benchmark.py to print help message upon incorrect execution of the code.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CAFABench",
+    version="1.0.7",
+    author="Iddo Friedberg Lab",
+    author_email="parnal@iastate.edu",
+    description="A program to generate benchmark for the CAFA challenge",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/CAFA-Challenge/CAFA_benchmark",
+    packages=setuptools.find_packages(),
+    #packages=["lib\create_benchmark"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+            "console_scripts": [
+                "CAFABench=CAFABench.create_benchmark:main",    
+            ],
+    },
+)
