60910ee69387cef2bd2b80bbf4f25a2cb36b842c
Joshi <parnal@iastate.edu>
2021-11-11 12:56:03 -0600

    Added unittest

--- setup.py
+++ setup.py
@@ -5,23 +5,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CAFABench",
-    version="1.0.10",
+    version="1.0.11",
     author="Iddo Friedberg Lab",
     author_email="parnal@iastate.edu",
     description="A program to generate benchmark for the CAFA challenge",
-    #long_description=long_description,
-    #long_description_content_type="text/markdown",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
     url="https://github.com/CAFA-Challenge/CAFA_benchmark",
     packages=setuptools.find_packages(),
-    #packages=["lib\create_benchmark"],
+    # packages=["lib\create_benchmark"],
+    test_suite="tests.test_create_benchmark",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     entry_points={
-            "console_scripts": [
-                "CAFABench=CAFABench.create_benchmark:main",    
-            ],
+        "console_scripts": [
+            "CAFABench=CAFABench.create_benchmark:main",
+        ],
     },
 )
---
+++ tests/test_create_benchmark.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+"""
+Program: Unit testing
+Author : Huy Nguyen
+Start  : 05/31/2017
+End    : /2017
+"""
+import unittest
+import os
+from CAFABench import create_benchmark
+
+# from create_benchmark import read_gaf,analyze
+
+cwd = os.getcwd()
+t1_dic, all_protein_t1 = create_benchmark.read_gaf(cwd+"//tests//1")
+t2_dic, all_protein_t2 = create_benchmark.read_gaf(cwd+"//tests//2")
+NK_dic, LK_dic = create_benchmark.analyze(t1_dic, t2_dic, all_protein_t1)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestBenchmark)
+    return suite
+
+
+class TestBenchmark(unittest.TestCase):
+    def test_not_include(self):
+        for protein in ["A2P2R3", "A2P2R4", "A2P2R5", "A2P2R6"]:
+            for ontology in NK_dic:
+                self.assertNotIn(protein, NK_dic[ontology], "This protein should not be in NK ")
+        for protein in ["A2P2R3", "A2P2R4", "A2P2R5", "A2P2R6"]:
+            for ontology in LK_dic:
+                self.assertNotIn(protein, LK_dic[ontology], "This protein should not be in LK ")
+
+    def test_include_NK(self):
+        self.assertIn("A5Z2X5", NK_dic["F"], "protein A5Z2X5 should gain knowledge in mfo in NK")
+        self.assertIn("A5Z2X5", NK_dic["C"], "protein A5Z2X5 should gain knowledge in cco in NK")
+        self.assertIn("A5Z2X5", NK_dic["P"], "protein A5Z2X5 should gain knowledge in bpo in NK")
+
+        self.assertIn("I2HB52", NK_dic["F"], "protein I2HB52 should gain knowledge in mfo in NK")
+        self.assertIn("I2HB52", NK_dic["C"], "protein I2HB52 should gain knowledge in cco in NK")
+        self.assertNotIn("I2HB52", NK_dic["P"], "protein I2HB52 should not gain knowledge in bpo in NK")
+
+        self.assertNotIn("I2HB70", NK_dic["F"], "protein I2HB70 should not gain knowledge in mfo in NK")
+        self.assertNotIn("I2HB70", NK_dic["C"], "protein I2HB70 should not gain knowledge in cco in NK")
+        self.assertIn("I2HB70", NK_dic["P"], "protein I2HB70 should gain knowledge in bpo")
+
+    def test_include_LK(self):
+        self.assertIn("O14464", LK_dic["F"], "protein O14464 should gain knowledge in mfo in LK")
+        self.assertIn("O14464", LK_dic["C"], "protein O14464 should gain knowledge in cco in LK")
+        self.assertNotIn("O14464", LK_dic["P"], "protein O14464 should not gain knowledge in bpo in LK")
+
+        self.assertNotIn("O13563", LK_dic["F"], "protein O13563 should not gain knowledge in mfo in LK")
+        self.assertNotIn("O13563", LK_dic["C"], "protein O13563 should not gain knowledge in cco in LK")
+        self.assertIn("O13563", LK_dic["P"], "protein O13563 should gain knowledge in bpo in LK")
+        # self.assertIn("O13564", LK_dic["P"], "protein O13563 should gain knowledge in bpo in LK")
+
+
+if __name__ == '__main__':
+    unittest.main()
